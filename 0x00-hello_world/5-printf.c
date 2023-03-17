744f730b279fcf7b74ff85ef0a0d6a374b511c62
rosalineachieng6 <rosalineachieng62gmail.com>
2023-03-16 18:13:48 -0700

    printing exactly

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main -Printing exactly
+ *
+ * Return: 0 ALWAYS
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
