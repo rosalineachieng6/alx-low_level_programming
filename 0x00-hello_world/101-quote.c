9650646c885aadd5852623c027462871bb001e30
rosalineachieng6 <rosalineachieng62gmail.com>
2023-03-16 19:54:43 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,2 +1,11 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+ * main - Return program
+ * Return: 1
+ */
+int main(void)
+{
+	putchar("and that piece of art is useful" - Dora Korpar, 2015 - 10 - 19"\n);
+	return (1);
+}
 
