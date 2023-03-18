658740bf5b71a86502163d10e90a39338e446c24
rosalineachieng6 <rosalineachieng62gmail.com>
2023-03-18 08:47:40 -0700

    task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - main function
+ *main - main function
  *
  * Return: Always 0
  */
@@ -12,14 +12,13 @@ int main(void)
 
 	for (i = 0 ; i < 10 ; i++)
 	{
-		if (i == 9)
-			putchar(i + '0');
-		else
+		putchar(i + '0');
+		if (i != 9)
 		{
-			putchar(i + '0');
 			putchar(',');
-			putchar(' ');
+			putchar(',');
 		}
 	}
+	putchar('\n');
 	return (0);
 }
