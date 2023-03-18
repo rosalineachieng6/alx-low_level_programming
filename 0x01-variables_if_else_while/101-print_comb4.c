5b6da81dcc77371046f8a9a2c43927cf0af5bd98
rosalineachieng6 <rosalineachieng62gmail.com>
2023-03-18 12:51:04 -0700

    printing numbers

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -12,18 +12,19 @@ int main(void)
 	int j;
 	int k;
 
-	for (i = 0 ; i < 10 ; i++)
+	for (i = 0 ; i < 9 ; i++)
 	{
-		for (j = 1 ; j < 10 ; i++)
+		for (j = i + 1 ; j <= 9 ; j++)
 		{
-			for (k = 2 ; k < 10 ; k++)
+			for (k = j + 1 ; k <= 9 ; k++)
 			{
-				if (i < j && j < k)
+				if ((j != i) != k)
 				{
-					putchar(i + '0');
-					putchar(j + '0');
-					putchar(k + '0');
-					if (i + j + k != 24)
+					putchar(i);
+					putchar(j);
+					putchar(k);
+					if (i == 7 && j == 8)
+					continue;
 					{
 						putchar(',');
 						putchar(' ');
