6bd66a02c68fa071283944050f587e69b47f8f66
rosalineachieng6 <rosalineachieng62gmail.com>
2023-03-20 06:57:30 -0700

    printing numbers

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -12,19 +12,18 @@ int main(void)
 	int j;
 	int k;
 
-	for (i = 0 ; i < 9 ; i++)
+	for (i = 0 ; i < 10 ; i++)
 	{
-		for (j = i + 1 ; j <= 9 ; j++)
+		for (j = 0 ; j < 10 ; j++)
 		{
-			for (k = j + 1 ; k <= 9 ; k++)
+			for (k = 0 ; k < 10 ; k++)
 			{
-				if ((j != i) != k)
+				if (i < j && j < k)
 				{
-					putchar(i);
-					putchar(j);
-					putchar(k);
-					if (i == 7 && j == 8)
-					continue;
+					putchar(i + '0');
+					putchar(j + '0');
+					putchar(k + '0');
+					if (i + j + k != 24)
 					{
 						putchar(',');
 						putchar(' ');
@@ -33,6 +32,6 @@ int main(void)
 			}
 		}
 	}
-		putchar('\n');
-		return (0);
+	putchar('\n');
+	return (0);
 }
