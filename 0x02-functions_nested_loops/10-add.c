c490007496d637f50fced2e870d65098cddda179
rosalineachieng6 <rosalineachieng62gmail.com>
2023-03-21 15:53:44 -0700

    adding

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - add two numbers
+ * @a: the firt number parameter
+ * @b: the second number parameter
+ * Return: the addition of both
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
