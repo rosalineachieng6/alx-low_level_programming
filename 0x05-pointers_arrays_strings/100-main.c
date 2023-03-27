3a97a18b4fc78cb9326b4222e26c3f4ff10df8dd
rosalineachieng6 <rosalineachieng62gmail.com>
2023-03-27 05:48:36 -0700

    main

--- 0x05-pointers_arrays_strings/100-main.c
+++ 0x05-pointers_arrays_strings/100-main.c
@@ -22,7 +22,8 @@ int main(void)
 	printf("%d\n", nb);
 	nb = _atoi("Suite 402");
 	printf("%d\n", nb);
-	nb = _atoi("         +      +    -    -98 Battery Street; San Francisco, CA 94111 - USA             ");
+	nb = _atoi("\n         +      +    -    -98 Battery Street;");
+	nb = _atoi("\n	San Francisco, CA 94111 - USA             ");
 	printf("%d\n", nb);
 	nb = _atoi("---++++ -++ Sui - te -   402 #cisfun :)");
 	printf("%d\n", nb);
